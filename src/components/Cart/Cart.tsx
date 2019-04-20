791aadb0b0dc939d14378a73d458458ec32787dd
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-04-20 14:06:49 +0100

    Display cart value with two digits after a decimal point.

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -9,7 +9,7 @@ interface ICartProps {
 function Cart({ cart }: ICartProps) {
   return (
     <div className="cart">
-      <h4>Cart: {cart.count}, {cart.value}</h4>
+      <h4>Cart: {cart.count}, {cart.value.toFixed(2)}</h4>
     </div>
   );
 }

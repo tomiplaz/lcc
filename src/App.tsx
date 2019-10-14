f042979544a5c16059ad4585a4ea3b3126fc4780
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-10-14 23:32:39 +0200

    Fetch products in App component and update store.

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,54 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Products from './containers/Products';
 import Cart from './containers/Cart';
+import { fetchProductsStart, fetchProductsSuccess, fetchProductsFailure, AppAction } from './store/app/actions';
+import { IProduct } from './types/Product';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <>
-        <Header />
-        <Cart />
-        <Products />
-        <Footer />
-      </>
-    );
+export interface IAppProps {
+  dispatchFetchProductsStart: () => void;
+  dispatchFetchProductsSuccess: (products: IProduct[]) => void;
+  dispatchFetchProductsFailure: () => void;
+}
+
+function App ({ dispatchFetchProductsStart, dispatchFetchProductsSuccess, dispatchFetchProductsFailure }: IAppProps) {
+  React.useEffect(() => {
+    dispatchFetchProductsStart();
+    fetch('http://localhost:3000/products')
+      .then(res => {
+        res.json().then((products: IProduct[]) => {
+          dispatchFetchProductsSuccess(products)
+        })
+      })
+      .catch(err => {
+        console.log('Error fetching products:', err)
+        dispatchFetchProductsFailure()
+      })
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <Cart />
+      <Products />
+      <Footer />
+    </>
+  );
+}
+
+function mapDispatchToProps (dispatch: Dispatch<AppAction>) {
+  return {
+    dispatchFetchProductsStart: () => {
+      dispatch(fetchProductsStart())
+    },
+    dispatchFetchProductsSuccess: (products: IProduct[]) => {
+      dispatch(fetchProductsSuccess(products))
+    },
+    dispatchFetchProductsFailure: () => {
+      dispatch(fetchProductsFailure())
+    }
   }
 }
 
-export default App;
+export default connect(undefined, mapDispatchToProps)(App);

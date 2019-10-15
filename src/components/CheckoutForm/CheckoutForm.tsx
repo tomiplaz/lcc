8390951a01436395a4e3a3e97d0ea692cb6b383a
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-10-16 00:19:37 +0200

    Add CheckoutForm component.

---
+++ src/components/CheckoutForm/CheckoutForm.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import { CardElement, injectStripe, ReactStripeElements } from 'react-stripe-elements'
+import './CheckoutForm.css'
+
+export interface ICheckoutFormProps {
+  stripe?: ReactStripeElements.StripeProps
+}
+
+function CheckoutForm ({ stripe }: ICheckoutFormProps) {
+  return (
+    <div className='checkout-form'>
+      <p>Would you like to complete the purchase?</p>
+      <CardElement />
+      <button onClick={submit}>Purchase</button>
+    </div>
+  )
+
+  async function submit () {
+    const { token } = await stripe.createToken({ name: 'Name' })
+    const response = await fetch('/charge', {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain' },
+      body: token.id
+    })
+
+    if (response.ok) {
+      console.log('Purchase successful!')
+    }
+  }
+}
+
+export default injectStripe(CheckoutForm)

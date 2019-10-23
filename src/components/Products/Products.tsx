4c519a0a1dc52a52cc80bdfb49ca29214d1fc3c6
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2019-10-23 23:16:56 +0200

    Implement selecting order and use ProductsControls component instead.

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -3,7 +3,10 @@ import { ICartItem } from 'src/types/Cart';
 import './Products.css';
 import ProductsContext from '../../contexts/ProductsContext'
 import ProductsList from '../ProductsList/ProductsList';
-import ProductsSort, { ProductsSortEnum } from '../ProductsSort/ProductsSort';
+import { DEFAULT_SORT_VALUE } from '../ProductsSort/ProductsSort';
+import { DEFAULT_ORDER_VALUE, ProductsOrderEnum } from '../ProductsOrder/ProductsOrder';
+import ProductsControls from '../ProductsControls/ProductsControls';
+import { IProduct } from 'src/types/Product';
 
 export interface IProductsProps {
   addToCart: (cartItem: ICartItem) => void;
@@ -12,18 +15,32 @@ export interface IProductsProps {
 export default Products;
 
 function Products({ addToCart }: IProductsProps) {
-  const [sort, setSort] = React.useState<string>(ProductsSortEnum.Name)
+  const [sort, setSort] = React.useState<string>(DEFAULT_SORT_VALUE)
+  const [order, setOrder] = React.useState<string>(DEFAULT_ORDER_VALUE)
   const products = React.useContext(ProductsContext)
-  const sortedProducts = [...products].sort((a, b) => a[sort] - b[sort])
+  const sortedProducts = [...products].sort(sortProducts)
 
   return (
-    <section>
-      <ProductsSort onChange={onSortChange} />
+    <section className='products'>
+      <ProductsControls
+        onSortChange={onSortChange}
+        onOrderChange={onOrderChange}
+      />
       <ProductsList products={sortedProducts} />
     </section>
-  );
+  )
+
+  function sortProducts (productA: IProduct, productB: IProduct) {
+    return order === ProductsOrderEnum.Ascending
+      ? productA[sort] - productB[sort]
+      : productB[sort] - productA[sort]
+  }
 
   function onSortChange (event: React.ChangeEvent<HTMLSelectElement>) {
     setSort(event.target.value)
+  }
+
+  function onOrderChange (event: React.ChangeEvent<HTMLSelectElement>) {
+    setOrder(event.target.value)
   }
 }
